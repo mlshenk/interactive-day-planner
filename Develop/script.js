1922e1f1cc80f5eaacb0b28a17cab2d9c871d6b4
Mike Shenk <mlshenk415@gmail.com>
2020-06-19 08:47:52 -0700

    almost complete, working to retain local storage

--- Develop/script.js
+++ Develop/script.js
@@ -28,28 +28,15 @@ for (i = 9; i <= 17 ; i++) {
     } else {
         $("#" + z + "h").addClass("present");
     }
-    //not sure why this makes my col-8 look like a col-2
-
-
-//loop for saving textArea input when clicked
-$("button").click(function(){
-for (i=0; i<buttons.length; i++) {
-     if(buttons[i]){};
-}
-});
-     //if id of save button is clicked, save that text area's content to local storage
-
-
-
-//if(textAreas[i].attributes[0].nodeValue === (moment().format("ha"))) {
-          //console.log("true")
-          //(textArea+i).attr("class","past")
-     //} from underneath the loop w
-
-
-//if current time < block time, color = green
-//if current time = block time, color = red
-//if current time > block tim, coloe = grey
-
-//to save to local storage... localStorage.setItem("todo", textArea[i].textContent) 
-    }
+  
+    };
+    
+    var storageArray = JSON.parse(localStorage.getItem("user-input")) || [];
+    $("button").click(function() {
+        for (let i = 0; i < 9; i++) {
+            storageArray.push($("textarea")[i].value); //how do we do this so we don't push back into the array (since I'm not trying to save old data??)
+        }
+        localStorage.setItem("user-input", JSON.stringify(storageArray));
+    })
+    console.log(storageArray);
+ 
